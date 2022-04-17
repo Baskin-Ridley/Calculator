eb280251bcd4abe55af6190633bf78a542989239
IPierrot123 <gbaskinridley@gmail.com>
2022-04-17 08:47:45 -0700

    Added operate function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log("test")
 
 function addition(firstValue, secondValue) {
+    console.log("hello");
     return (firstValue + secondValue)
 }
 
@@ -15,3 +16,20 @@ function multiply(firstValue, secondValue) {
 function divide(firstValue, secondValue){
     return (firstValue / secondValue)
 }
+
+function operate(operator, a, b) {
+    switch (operator) {
+      case "+":
+        return addition(a, b)
+      case "−":
+        return substract(a, b)
+      case "×":
+        return multiply(a, b)
+      case "÷":
+        if (b === 0) return null
+        else return divide(a, b)
+      default:
+        return null
+    }
+}
+
