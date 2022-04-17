8f5962e15e6ff0a7b7e0f070a13a763239322a55
IPierrot123 <gbaskinridley@gmail.com>
2022-04-17 07:51:23 -0700

    Created add,sub,divide,multiply functions

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log("test")
+
+function addition(firstValue, secondValue) {
+    return (firstValue + secondValue)
+}
+
+function subtraction(firstValue, secondValue) {
+    return (firstValue - secondValue)
+}
+
+function multiply(firstValue, secondValue) {
+    return (firstValue * secondValue)
+}
+
+function divide(firstValue, secondValue){
+    return (firstValue / secondValue)
+}
